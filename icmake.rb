ff79b989ec00717b8aa53cf21d94efd7a9d34ab3
Andrew Janke <andrew@apjanke.net>
2015-10-05 06:35:49 -0400

    icmake, yodl: fix up to get them building

--- icmake.rb
+++ icmake.rb
@@ -1,5 +1,5 @@
 class Icmake < Formula
-  desc "Icmake: a make alternative"
+  desc "Icmake: a `make` alternative"
   homepage "http://icmake.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/icmake/icmake/7.22.01/icmake_7.22.01.orig.tar.gz"
   sha256 "b522e7937e9d4f0bec738dfce371673e3c4a8bc9f4d209a51631e5ed59ba66c7"
@@ -9,44 +9,35 @@ class Icmake < Formula
   patch :DATA
 
   def install
-    inreplace "INSTALL.im" do |s|
-      s.gsub! /usr/, prefix
-      s.gsub! /etc/, "#{prefix}/etc"
-    end
+    # Completely overwrite their config file, since it is not programatically
+    # configurable
+    (buildpath/"INSTALL.im").atomic_write <<-EOS.undent
+      #define BINDIR      "#{bin}"
+      #define SKELDIR     "#{share}/icmake"
+      #define MANDIR      "#{man}"
+      #define LIBDIR      "#{libexec}"
+      #define CONFDIR     "#{etc}/icmake"
+      #define DOCDIR      "#{doc}"
+      #define DOCDOCDIR   "#{doc}/icmake-doc"
+    EOS
 
-    inreplace "icm_prepare" do |s|
-      s.gsub! %r{\. scripts/conversions\n\n}, ""
+    # icmake's sed calls require GNU-specific behavior
+    inreplace ["scripts/convert", "scripts/conversions", "scripts/icmstart.in"] do |s|
+      s.gsub! /\bsed\b/, "gsed"
     end
-
-    inreplace "icm_install" do |s|
-      s.gsub! %r{\. scripts/conversions\n\n}, ""
-    end
-
-    if `/usr/bin/which gsed`
-      opoo "Using gnu-sed"
-      inreplace "scripts/convert" do |s|
-        s.gsub! /sed/, "gsed"
-        s.gsub! %r{\. scripts/conversions\n\n}, ""
-      end
-    end
-
-    ENV["ROOT"] = ""
-    ENV["BINDIR"] = bin
-    ENV["SKELDIR"] = "#{share}/icmake"
-    ENV["MANDIR"] = "#{man}"
-    ENV["LIBDIR"] = "#{lib}/icmake"
-    ENV["CONFDIR"] = "#{prefix}/etc"
-    ENV["DOCDIR"] = "#{share}/doc/icmake"
-    ENV["DOCDOCDIR"] = "#{share}/doc/icmake/icmake-doc"
-    ENV["VERSION"] = `cat VERSION | grep VERSION | awk -F '[= ]*'' '{print $2}'`.chomp
-    ENV["YEARS"] = `cat VERSION | grep YEARS | awk -v RS="" -F '[= ]*'' '{print $2}'`.chomp
 
     # lexer.c should be rebuilt, because it was created for a linux system.
     # See also the patch.
-    system "/usr/bin/flex", "-o", "comp/lexer.c", "comp/lexer"
+    system "flex", "-o", "comp/lexer.c", "comp/lexer"
     system "./icm_bootstrap", "/"
 
     system "./icm_install", "all"
+  end
+
+  test do
+    # Have to use "; true" because their `-a` returns error status
+    assert_match /ICMAKE consists of a set of five programs/,
+      shell_output("icmake -a 2>&1; true")
   end
 end
 
